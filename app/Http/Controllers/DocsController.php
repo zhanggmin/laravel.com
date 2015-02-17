ee60e83fd3d743df235d9787b7f2e0b3cfb5260a
Charlie Jade <summer.alex07@gmail.com>
2015-02-17 08:52:54 +0800

    only support 4.2 and 5.0

--- app/Http/Controllers/DocsController.php
+++ app/Http/Controllers/DocsController.php
@@ -76,11 +76,8 @@ class DocsController extends Controller {
 	protected function getDocVersions()
 	{
 		return [
-			'master' => 'Master',
 			'5.0' => '5.0',
-			'4.2' => '4.2',
-			'4.1' => '4.1',
-			'4.0' => '4.0',
+			'4.2' => '4.2'
 		];
 	}
 
