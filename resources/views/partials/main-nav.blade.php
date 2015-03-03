95711ae5a82fdf78ae3b392c9efd6ebd890dfa28
Charlie Jade <summer.alex07@gmail.com>
2015-03-04 07:58:59 +0800

    update navigation

--- resources/views/partials/main-nav.blade.php
+++ resources/views/partials/main-nav.blade.php
@@ -1,11 +1,11 @@
 <li class="nav-docs"><a href="/docs">文档</a></li>
-<li class="nav-laracasts"><a href="https://laracasts.com">Laracasts</a></li>
-<li class="nav-forge"><a href="https://forge.laravel.com">Forge</a></li>
-<li class="nav-api" style="display:none"><a href="/api/5.0">API</a></li>
+<li class="nav-laracasts"><a href="https://laracasts.com" target="_blank">Laracasts</a></li>
+<li class="nav-forge"><a href="https://forge.laravel.com" target="_blank">Forge</a></li>
+<li class="nav-api" style="display:none"><a href="/api/5.0" target="_blank">API</a></li>
 <li class="dropdown">
-	<a href="https://phphub.org/">中文开发者社区</a>
+	<a href="https://phphub.org/" target="_blank">中文开发者社区</a>
 </li>
 <li class="dropdown">
-	<a href="https://phphub.org/topics/453">Slack 聊天室</a>
+	<a href="https://phphub.org/topics/453" target="_blank">Slack 聊天室</a>
 </li>
 <a href="https://github.com/laravel-china/docs" target="_blank"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/a6677b08c955af8400f44c6298f40e7d19cc5b2d/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f677261795f3664366436642e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png"></a>
