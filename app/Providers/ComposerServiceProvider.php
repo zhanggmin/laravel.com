c11a0954139acbfb1e0bf54548d9a0ca9730bd6f
lifesign <leocloudy@gmail.com>
2015-03-28 18:52:33 +0800

    fix doc update time not corresponding to the version

--- app/Providers/ComposerServiceProvider.php
+++ app/Providers/ComposerServiceProvider.php
@@ -2,26 +2,29 @@
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Contracts\Cache\Repository as Cache;
+use Illuminate\Http\Request;
 use GuzzleHttp\Client;
 use Carbon\Carbon;
 
 class ComposerServiceProvider extends ServiceProvider {
 
-    const DOC_API = 'https://api.github.com/repos/laravel-china/docs/commits?per_page=1';
+    const DOC_API = 'https://api.github.com/repos/laravel-china/docs/commits?per_page=1&sha=%s';
 
     /**
      * Bootstrap the application services.
      *
      * @return void
      */
-    public function boot(Client $client, Cache $cache)
+    public function boot(Client $client, Cache $cache, Request $request)
     {
         $client->setDefaultOption('verify', false);
 
-        $this->app['view']->composer('docs',function($view) use($client, $cache){
+        $this->app['view']->composer('docs',function($view) use($client, $cache, $request){
 
-            $view->update_date = $cache->remember('doc_update_at', 24 * 60, function() use ($client) {
-                $response = $client->get(self::DOC_API)->json();
+            $version = $request->segment(2) ?: DEFAULT_VERSION;
+
+            $view->update_date = $cache->remember("doc_{$version}_update_at", 24 * 60, function() use ($client, $version) {
+                $response = $client->get(sprintf(self::DOC_API, $version))->json();
                 if (isset($response[0]['commit']['author']['date'])) {
                     return Carbon::parse($response[0]['commit']['author']['date'])->toDateString();
                 }
