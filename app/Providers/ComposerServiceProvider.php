96a980e94c28f46074ecd8b173cdd19812986e3b
lifesign <leocloudy@gmail.com>
2015-03-28 19:02:41 +0800

    cs

--- app/Providers/ComposerServiceProvider.php
+++ app/Providers/ComposerServiceProvider.php
@@ -1,4 +1,6 @@
-<?php namespace App\Providers;
+<?php
+
+namespace app\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Contracts\Cache\Repository as Cache;
@@ -6,24 +8,22 @@ use Illuminate\Http\Request;
 use GuzzleHttp\Client;
 use Carbon\Carbon;
 
-class ComposerServiceProvider extends ServiceProvider {
-
+class ComposerServiceProvider extends ServiceProvider
+{
     const DOC_API = 'https://api.github.com/repos/laravel-china/docs/commits?per_page=1&sha=%s';
 
     /**
      * Bootstrap the application services.
-     *
-     * @return void
      */
     public function boot(Client $client, Cache $cache, Request $request)
     {
         $client->setDefaultOption('verify', false);
 
-        $this->app['view']->composer('docs',function($view) use($client, $cache, $request){
+        $this->app['view']->composer('docs', function ($view) use ($client, $cache, $request) {
 
             $version = $request->segment(2) ?: DEFAULT_VERSION;
 
-            $view->update_date = $cache->remember("doc_{$version}_update_at", 24 * 60, function() use ($client, $version) {
+            $view->update_date = $cache->remember("doc_{$version}_update_at", 24 * 60, function () use ($client, $version) {
                 $response = $client->get(sprintf(self::DOC_API, $version))->json();
                 if (isset($response[0]['commit']['author']['date'])) {
                     return Carbon::parse($response[0]['commit']['author']['date'])->toDateString();
@@ -37,12 +37,9 @@ class ComposerServiceProvider extends ServiceProvider {
 
     /**
      * Register the application services.
-     *
-     * @return void
      */
     public function register()
     {
         //
     }
-
 }
