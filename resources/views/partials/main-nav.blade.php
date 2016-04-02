20036b2a751333cbb7954adc57c945623b56f79e
Charlie Jade <summer.alex07@gmail.com>
2016-04-02 11:40:54 +0800

    update top nav

--- resources/views/partials/main-nav.blade.php
+++ resources/views/partials/main-nav.blade.php
@@ -4,8 +4,5 @@
 <li class="nav-forge"><a href="https://forge.laravel.com" target="_blank">Forge</a></li>
 <li class="nav-api" style="display:none"><a href="/api/5.0" target="_blank">API</a></li>
 <li class="dropdown">
-	<a href="https://phphub.org/" target="_blank">中文开发者社区</a>
-</li>
-<li class="dropdown">
-	<a href="https://phphub.org/topics/453" target="_blank">Slack 聊天室</a>
+	<a href="https://phphub.org/" target="_blank">Laravel 社区</a>
 </li>
