3abf4838b37a22ee102e98a84b44daa8860a8f3a
Charlie Jade <summer.alex07@gmail.com>
2016-04-05 22:33:50 +0800

    update top nav

--- resources/views/partials/main-nav.blade.php
+++ resources/views/partials/main-nav.blade.php
@@ -1,8 +1,7 @@
 <li class="nav-docs"><a href="/docs">文档</a></li>
 <li class="nav-laracasts"><a href="https://laracasts.com" target="_blank">Laracasts</a></li>
 <li class="nav-docs"><a href="http://lumen.laravel-china.org">Lumen</a></li>
-<li class="nav-forge"><a href="https://forge.laravel.com" target="_blank">Forge</a></li>
-<li class="nav-api" style="display:none"><a href="/api/5.0" target="_blank">API</a></li>
+<li class="nav-forge"><a href="https://cs.phphub.org/" target="_blank">Laravel LTS 速查表</a></li>
 <li class="dropdown">
 	<a href="https://phphub.org/" target="_blank">Laravel 社区</a>
 </li>
